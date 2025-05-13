9014ca726f126d826d6e2c210be77edcf9373569
Delfina Munich <delfinamunich@gmail.com>
2025-05-13 20:24:59 -0300

    paginas

---
+++ farmacity 24-7/src/pages/checkout.vue
@@ -0,0 +1,1 @@
+import paymentMethod from '@/components/paymentMethod.vue'
--- farmacity 24-7/vite.config.js
+++ farmacity 24-7/vite.config.js
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import tailwindcss from '@tailwindcss/vite'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -8,7 +9,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    vueDevTools(), tailwindcss()
   ],
   resolve: {
     alias: {
