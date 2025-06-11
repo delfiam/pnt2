f8ec3e1fb8d09090090f2f5c921fbe206b038c4a
Delfina Munich <delfinamunich@gmail.com>
2025-06-10 21:29:55 -0300

    medicamentos a api

---
+++ farmacity 24-7/src/components/medicamentosCard.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps({
+    medicamento: {
+        type: Object,
+        required: true
+    }
+})
+</script>
+
+<template>
+ 
+    <div class="card">
+        <img :src="medicamento.imagen" class="card-img-top" alt="Imagen del medicamento">
+        <div class="card-body">
+        <h5 class="card-title">{{ medicamento.nombre }}</h5>
+        <p class="card-text">Precio: ${{ medicamento.precio }}</p>
+        <p class="card-text">Descripción: {{ medicamento.descripcion }}</p>
+        <button class="btn btn-primary" @click="$emit('agregar-al-carrito', medicamento)">Agregar al carrito</button>
+        </div>
+    </div>
+</template>
--- farmacity 24-7/src/pages/Home.vue
+++ farmacity 24-7/src/pages/Home.vue
@@ -1,10 +1,27 @@
+<script setup>
+import medicamentosCard from '@/components/medicamentosCard.vue';
+//import medicamentos from '@/assets/medicamentos.json';
+
+import axios from 'axios';
+const medicamentos = ref([]);
+import { ref, onMounted } from 'vue';
+onMounted(async () => {
+    try {
+        const response = await axios.get('https://682d2cee4fae188947551d71.mockapi.io/stock/stock');
+        medicamentos.value = response.data;
+    } catch (error) {
+        console.error('Error fetching medicamentos:', error);
+    }
+});
+</script>
 <template>
     <div class="container">
+        <h1>Bienvenido a Farmacity</h1>
+        <h2>Medicamentos</h2>
         <div class="row">
-        <div class="col-md-12">
-            <h1 class="text-center my-4">Farmacity 24/7</h1>
-            <p class="text-center">Tu salud, nuestra prioridad.</p>
+        <div class="col-md-4" v-for="medicamento in medicamentos" :key="medicamento.id">
+            <medicamentosCard :medicamento="medicamento" />
         </div>
-    </div>
+        </div>
 </div>
 </template>
