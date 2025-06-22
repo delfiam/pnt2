2af550dd77f7e34db7702c776626c9465fede2ce
Delfina Munich <89984382+delfiam@users.noreply.github.com>
2025-06-21 22:49:00 -0300

    Update medicamentosCard.vue

--- farmacity 24-7/src/components/medicamentosCard.vue
+++ farmacity 24-7/src/components/medicamentosCard.vue
@@ -24,7 +24,7 @@ function handleAgregar() {
 
     <div v-if="medicamento.oferta"
       class="absolute top-2 left-2 bg-red-600 text-white text-xs font-bold px-2 py-1 rounded shadow">
-      -{{ medicamento.oferta }} Oferta
+      -{{ medicamento.descuento * 100 }}%
     </div>
 
     <div>
